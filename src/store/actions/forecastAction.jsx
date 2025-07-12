9f69bcfcd65f3fd7ab4b7604fb76bd5484a27612
Akki-b2008 <rathoreakash2356@gmail.com>
2025-07-12 23:30:54 +0530

    Home page ui and data fetched done.

--- src/store/actions/forecastAction.jsx
+++ src/store/actions/forecastAction.jsx
@@ -2,8 +2,10 @@ import axios from "../../utils/axiosconfig";
 import { loadforecast } from "../reducers/forecastSlice";
 
 export const asyncforecastDetails = (city) => async(dispatch) => {
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
   try {
-    const { data } =await axios.get(`/forecast?q=${city}&units=metric&appid=da4194102ae0dc773e53ad7a8baa167a`
+    const { data } =await axios.get(`/forecast?q=${city}&units=metric&appid=${API_KEY}`
     );
     dispatch(loadforecast(data))
   } catch (error) {
--- src/store/actions/weatherAction.jsx
+++ src/store/actions/weatherAction.jsx
@@ -2,9 +2,11 @@ import axios from "../../utils/axiosconfig";
 import { loadweather } from "../reducers/weatherSlice";
 
 const asyncWeatherDetails = (city) => async (dispatch) => {
+    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
   try {
     const {data}= await axios.get(
-      `/weather?q=${city}&units=metric&appid=da4194102ae0dc773e53ad7a8baa167a`
+      `/weather?q=${city}&units=metric&appid=${API_KEY}`
     );
     localStorage.setItem("city", JSON.stringify(city));
     dispatch(loadweather(data));
